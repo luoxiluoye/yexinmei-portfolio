daf2e71707e6334da9cfdf0c31e8c25f059b884f
小白白 <111624310+luoxiluoye@users.noreply.github.com>
2026-09-05 11:42:34 +0800

    Simplify public contact options

--- data/contact.ts
+++ data/contact.ts
@@ -3,7 +3,6 @@ export const contact = {
   bubble: "想聊内容、新媒体、社区、摄影，或者只是分享一个有趣的东西，都可以来找我。",
   items: [
     { type: "email", label: "EMAIL", value: "2657351670@qq.com" },
-    { type: "phone", label: "PHONE", value: "18080409809" },
     { type: "wechat", label: "WECHAT", value: "luoxiluoye" },
     { type: "resume", label: "RESUME", value: "TODO" },
   ],
